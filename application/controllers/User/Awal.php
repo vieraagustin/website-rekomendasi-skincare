54caec48dd7ec9fee8c041a9c2b762e210f5d13f
vieraagustin <71694055+vieraagustin@users.noreply.github.com>
2022-01-22 22:14:11 +0700

    commit6

--- application/controllers/User/Awal.php
+++ application/controllers/User/Awal.php
@@ -4,12 +4,19 @@ class Awal extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
-		//$this->load->model('User/Awal_Model','Awal_Model');
+		$this->load->model('User/Awal_Model','Awal_Model');
 
 	}
 		public function index()
 		{
 			//$this->load->view('user/sidebar_user');
 			$this->load->view('user/halamanawal');
+		}
+		public function simpan_nama(){
+			$nama=$this->input->post('nama');
+			// $this->Awal_Model->simpan_nama
+			$this->session->set_userdata('nama',$nama);
+			var_dump($this->session->userdata('nama'));
+
 		}
 }

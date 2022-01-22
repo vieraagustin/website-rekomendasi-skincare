3b90334b47b053a0cf698a1812abe2f0f02b0158
vieraagustin <71694055+vieraagustin@users.noreply.github.com>
2022-01-22 22:16:51 +0700

    model awal

--- application/models/User/Awal_Model.php
+++ application/models/User/Awal_Model.php
@@ -1,6 +1,9 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
-class Awal extends CI_Model
+class Awal_Model extends CI_Model
 {
+	public function simpan_nama($data){
+		return $this->db->insert('data_pengguna',$data);
 
+	} 
 }

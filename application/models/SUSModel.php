1d0747b648729e17e70b6255358cdeec638a3d47
insanz01 <insankamil002@gmail.com>
2022-03-23 18:07:56 +0700

    Fix SUS

--- application/models/SUSModel.php
+++ application/models/SUSModel.php
@@ -2,14 +2,22 @@
 
 class SUSModel extends CI_Model {
 	public function getSkor() {
-		$query = "SELECT feedback_id, SUM(jawaban) as total FROM sus_feedback GROUP BY feedback_id";
+		$query = "SELECT feedback_id FROM sus_feedback GROUP BY feedback_id";
 
 		$feedbacks = $this->db->query($query)->result_array();
 
 		$totalSkor = 0;
 
 		foreach($feedbacks as $feedback) {
-			$totalSkor += (int) $feedback['total'] * 2;
+			$skors = $this->db->get_where('sus_feedback', ['feedback_id' => $feedback['feedback_id']])->result_array();
+
+			foreach ($skors as $skor) {
+				if($skor['id_soal'] % 2 == 0) {
+					$totalSkor += (int) $skor['jawaban'] - 5;
+				} else {
+					$totalSkor += (int) $skor['jawaban'] - 1;
+				}
+			}
 		}
 
 		return (float) $totalSkor / count($feedbacks);
